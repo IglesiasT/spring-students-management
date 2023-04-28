3833e37297e7e380076540d230a0ba0d87c8aab3
Tomás <iglesiastomas99@gmail.com>
2023-04-28 11:22:42 -0300

    delete tests

--- src/test/java/com/example/crudstudentsmanagement/UserTests.java
+++ src/test/java/com/example/crudstudentsmanagement/UserTests.java
@@ -15,38 +15,6 @@ class UserTests {
 
 	@Autowired
 	private UserService userService;
-	@Autowired
-	private BCryptPasswordEncoder encoder;
 
-	@Test
-	void createUserTest() {
-		// Arrange
-		String userName = "testUser1";
-		String password = "123";
-		UserModel newUser = new UserModel(userName, password);
-
-		// Act
-		this.userService.saveUser(newUser);
-
-		// Assert
-		UserModel user = userService.getUserById(newUser.getId());
-		assertNotNull(user);
-		assertEquals(userName, user.getUsername());
-		assertEquals(password, user.getPassword());
-	}
-
-	@Test
-	void encodePasswordTest(){
-		// Arrange
-		String userName = "testUser2";
-
-		// Act
-		String password = this.encoder.encode("4566");
-		UserModel newUser = new UserModel(userName, password);
-		this.userService.saveUser(newUser);
-
-		// Assert
-		assertEquals(newUser.getPassword(), this.userService.getUserById(newUser.getId()).getPassword());
-	}
 
 }
