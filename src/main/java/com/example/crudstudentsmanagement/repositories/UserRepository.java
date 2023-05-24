ec3be4da36334cb5b86f4212cf65f59df01a0467
Tomás <iglesiastomas99@gmail.com>
2023-05-24 10:04:22 -0300

    delete comments

--- src/main/java/com/example/crudstudentsmanagement/repositories/UserRepository.java
+++ src/main/java/com/example/crudstudentsmanagement/repositories/UserRepository.java
@@ -6,10 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
-    /*
-     al heredar de jparepository, Spring Data JPA sabe que debe implementar el metodo
-     porque sigue la convención de nombrar los métodos de consulta como "findBy" seguido
-     del nombre de la propiedad que se utilizará para realizar la búsqueda.
-     */
     UserModel findByUsername(String username);
 }
